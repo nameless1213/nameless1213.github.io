047572a951de9c5df172c5c6ce6572da2a03a907
nameless1213 <delux2006@live.com>
2024-02-21 09:50:20 +0800

    love

--- js/lovetime.js
+++ js/lovetime.js
@@ -23,7 +23,7 @@ function lovetime() {
     var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes)
     var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes *
         minutes) / seconds)
-    document.getElementById("lovetime").innerHTML = "我们已经在一起 " + toDays + "天" +
+    document.getElementById("lovetime").innerHTML =  toDays + "天" +
         diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒啦"
 }
 lovetime()
